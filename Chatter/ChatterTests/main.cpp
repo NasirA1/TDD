4ce2e560ad43cee040074a1a7a0a01838c7ace1b
NasirA1 <pusarlai@msn.com>
2017-08-14 23:26:48 +0100

    Added receive handler to the presenter

--- Chatter/ChatterTests/main.cpp
+++ Chatter/ChatterTests/main.cpp
@@ -375,7 +375,7 @@ IMPLEMENT_APP(MywxApp)
 /////////////////////////////////////////// TEST CODE //////////////////////////////////////////////
 
 
-#ifdef TESTING
+//#ifdef TESTING
 #include <gtest/gtest.h>
 #include <gmock/gmock.h>
 
@@ -585,7 +585,7 @@ TEST(ChatterPresenter, ReceivedMessageIAppendedToChatHistory)
 	//we expect that the message should appear in view2's chat history
 	//when it is received
 	EXPECT_CALL(view2, AppendToChatHistory("hi from channel1!\n"));
-	
+
 	channel1.SendMessage("hi from channel1!");
 
 	//busy wait until the message is received 
